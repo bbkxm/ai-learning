9562afd4d9525f49720a471fb55c6ec6cc65828c
bbkxm <1027191493@qq.com>
2024-02-24 20:14:57 +0800

    chatgpt prompt

---
+++ LangChain/lesson1/.ipynb_checkpoints/openai-checkpoint.py
@@ -0,0 +1,14 @@
+import os
+os.environ["OPENAI_API_KEY"] = 'sk-888'
+from openai import OpenAI
+client = OpenAI()
+response = client.chat.completions.create(  
+  model="gpt-3.5-turbo",
+  messages=[
+        {"role": "system", "content": "You are a creative AI."},
+        {"role": "user", "content": "请给我的花店起个名"},
+    ],
+  temperature=0.8,
+  max_tokens=60
+)
+print(response.choices[0].message.content)
